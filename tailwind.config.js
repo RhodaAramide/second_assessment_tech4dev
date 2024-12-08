7ecdeaad4b0116168971d8e487b0b4134c0749e7
oreoluwa212 <olabisiabolarin5@gmail.com>
2024-12-08 05:33:54 +0100

    Added navbar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        blueLight: "#4489FC",
+        blue: "#151083",
+        whiteDeep: "#D0D3DA",
+        whiteLight: "#FCFCFD",
+      },
+      fontSize: {
+        sm: ["14px", "16px"],
+        lg: ["64px", "82px"],
+      },
+    },
   },
   plugins: [],
 };
